676dff4ab38bfc0a190e6feaf6fb71b4fc0229ff
steynchris42-alt <steynchris42@gmail.com>
2026-04-04 15:27:39 +0200

    Uploaded scripts

---
+++ Assets/Scripts/Combat/Obstacles.cs
@@ -0,0 +1,61 @@
+using UnityEditor.Rendering;
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    //stores 5 hitpoints
+    int HitPoints = 5;
+    Rigidbody2D rb;
+    SpriteRenderer rs;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rs = GetComponent<SpriteRenderer>();
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullets"))
+        {
+            //subtracts one hit point on each collision.
+            HitPoints --;
+
+        
+        }
+
+        if (HitPoints == 4)
+        {
+          rs.color = new Color(0.6980392f, 0.1333333f, 0.1333333f, 1f);
+
+        }
+        if (HitPoints == 3)
+        {
+            rs.color = new Color(0.7686275f, 0.222598f, 0.1490196f, 1f);
+
+        }
+        if (HitPoints == 2)
+        {
+            rs.color = new Color(0.7686275f, 0.333261f, 0.1490196f, 1f);
+
+        }
+        if (HitPoints == 1)
+        {
+            rs.color = new Color(0.6980392f, 0.4548363f, 0.1333333f, 1f);
+
+        }
+
+        //destroys the obstacle if the hit points reach 0.
+        if (HitPoints == 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
+
