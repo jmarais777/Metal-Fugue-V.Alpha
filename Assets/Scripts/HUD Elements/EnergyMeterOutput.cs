de97ccd0debe5dbf53ca5984f9aaa6023be19ac7
jmarais777 <77393672+jmarais777@users.noreply.github.com>
2026-04-06 14:41:00 +0200

    Cortical Light Emitter
    Cortical Light Emitter

--- Assets/Scripts/HUD Elements/EnergyMeterOutput.cs
+++ Assets/Scripts/HUD Elements/EnergyMeterOutput.cs
@@ -3,6 +3,12 @@ using UnityEngine.UIElements;
 
 public class EnergyText : MonoBehaviour
 {
+    //Display CurrentPool on EnergyMeter
+    //Title: Unity UI Toolkit in 5 Minutes
+    //Author: MadCat Tutorials
+    //Date: 05/04/2026
+    //Availiability: https://www.youtube.com/watch?v=yUXFHAOXhcA
+
     //calling EnergyPool for Current Energy
     public EnergyPool ForCurrentEnergy;
 
