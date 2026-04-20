5e0b73f172e2593b55f3f2c492ab6be62271fb2a
steynchris42-alt <steynchris42@gmail.com>
2026-04-20 19:41:32 +0200

    push

---
+++ Assets/Scripts/UI/MainMenuEvents.cs
@@ -0,0 +1,33 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem.Composites;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+public class MainMenuEvents : MonoBehaviour
+{
+    private UIDocument document;
+    private Button button;
+   
+    private void Awake()
+    {
+        document = GetComponent<UIDocument>();
+        button = document.rootVisualElement.Q("Startgame") as Button;
+        button.RegisterCallback<ClickEvent>(OnPlayGameCLick);
+    }
+
+    private void OnDisable()
+    {
+        button.UnregisterCallback<ClickEvent>(OnPlayGameCLick);
+    }
+
+    private void OnPlayGameCLick(ClickEvent evt)
+    {
+
+      
+        SceneManager.LoadScene("Test");
+
+    }
+   
+
+}
+
