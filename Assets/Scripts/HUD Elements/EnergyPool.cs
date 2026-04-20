97ae795f28ba500e89984c80f48487e219077421
steynchris42-alt <steynchris42@gmail.com>
2026-04-20 14:30:29 +0200

    Testv2

--- Assets/Scripts/HUD Elements/EnergyPool.cs
+++ Assets/Scripts/HUD Elements/EnergyPool.cs
@@ -6,23 +6,27 @@ public class EnergyPool : MonoBehaviour
     //Calling PlayerMovement & ShootMech into this script (for the IsDashing & IsShooting variables, hence the names)
     public PlayerMovement ForIsDashing;
     public ShootMech ForIsShooting;
+    public PlayerHealth ForIsBeingShot;
+
 
     //Defining range of Energy Pool
     public float CurrentEnergy;
     public float MaxEnergy = 100f;
 
     bool wasDashing = false;
-    
+    public Collider2D player;
     //Establishing variables for energy drain based on distance travelled
     public Rigidbody2D RigBod;
     public float DistanceLimit = 12f;
     public float MovementDrain = 1f;
+    public float BulletDrain = 1f;
     Vector2 lastPoint;
 
     void Start()
     {
         ForIsDashing = GetComponent<PlayerMovement>();
         ForIsShooting = GetComponent<ShootMech>();
+        
         
         CurrentEnergy = MaxEnergy;
        
@@ -60,11 +64,23 @@ public class EnergyPool : MonoBehaviour
             CurrentEnergy = Mathf.Clamp(CurrentEnergy, 0, MaxEnergy);
             //Debug.Log(CurrentEnergy);
         }
+        if (ForIsBeingShot)
+            CurrentEnergy -= 1;
+        CurrentEnergy = Mathf.Clamp(CurrentEnergy, 0, MaxEnergy);
 
     if (CurrentEnergy == 0)
         {
             //Debug.Log("You Have Died");
         }
    
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("EnemyBullets"))
+        {
+            CurrentEnergy = CurrentEnergy - 1;
+            Debug.Log("Hit");
+
+        }
     }
 }
