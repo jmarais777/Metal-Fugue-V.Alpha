a7899d98d8e9bbacb7eaff2fb7dd8aa4602a6aa5
steynchris42-alt <steynchris42@gmail.com>
2026-05-01 14:20:08 +0200

    Player DIalogue menu system complete

--- Assets/Scripts/UI/Player_Dialogue1.cs
+++ Assets/Scripts/UI/Player_Dialogue1.cs
@@ -1,24 +1,107 @@
+using JetBrains.Annotations;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class Player_Dialogue1 : MonoBehaviour
 {
-    public UIDocument PlayerDialogue1;
 
+    public Label PlayerDialogueContainer;
+    public UIDocument PlayDiaUI1;
+    public VisualElement root;
+    public Button NextButton;
+    public GameObject PlayerDialogueMenu1;
+    public string[] PlayerDialogueLines;
+    public int DialogueIndex = 0;
+
+
+
+
+    private void Update()
+    {
+        if (PlayerDialogueMenu1.activeSelf == false)
+        {
+            if (Input.GetKeyDown(KeyCode.H))
+            {
+
+                ShowMenu1();
+
+            }
+        }
+       
+        
+    }
+
+
+        void ShowMenu1()
+        {
+        PlayerDialogueMenu1.SetActive(true);
     
-    void Start()
-    {
+      
+            Time.timeScale = 0.0f;
+            var Ui1 = PlayDiaUI1.GetComponent<UIDocument>();
+            if (Ui1 == null || Ui1.rootVisualElement == null)
+            {
+                return;
+
+            }
+            if (Ui1 != null)
+            {
+                var root = Ui1.rootVisualElement;
+                NextButton = root.Q<Button>("NextButton");
+                PlayerDialogueContainer = root.Q<Label>("PlayerDialogueContainer");
+            
+            }
         
+
+
+        
+        if (NextButton != null)
+        {
+            NextButton.clicked += NextButtonOnClick;
+            UpdateDialogueLines();
+        }
+     
+    
     }
 
- 
-    void Update()
+    void HideMenu1()
     {
-        
+        NextButton.SetEnabled(false);
+        PlayerDialogueMenu1.SetActive(false);
+    
+       // PlayDiaUI1.enabled = false;
+        Time.timeScale = 1.0f;
+        //PlayDiaUI1.enabled = false;
+       
+
     }
-    void ShowMenu1()
+
+
+    void NextButtonOnClick()
     {
 
+        DialogueIndex++;
+        UpdateDialogueLines();
 
+        Debug.Log("Clicking");
+
+
+    }
+    void UpdateDialogueLines()
+    {
+      
+        if (DialogueIndex < PlayerDialogueLines.Length)
+        {
+            PlayerDialogueContainer.text = PlayerDialogueLines[DialogueIndex];
+        }
+        else if(DialogueIndex > PlayerDialogueLines.Length)
+        {
+            HideMenu1();
+            return;
+        }
     }
 }
+
+  
