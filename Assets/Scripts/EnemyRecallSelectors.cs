a980f25fe7cdbc6c85ef4b47a03ef4be98690a5d
steynchris42-alt <steynchris42@gmail.com>
2026-05-11 23:23:28 +0200

    EnemyPathfiningStatemachine setup
    Setup colliders as triggers by tags.
    
    started the central control script of the state machine

--- Assets/Scripts/EnemyRecallSelectors.cs
+++ Assets/Scripts/EnemyRecallSelectors.cs
@@ -1,16 +1,32 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyRecallSelectors : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public enum  RecallLayout
+    {
+        Defualt = 0,
+        Linear = 1,
+        Wide = 2,
+    }
+    public RecallLayout option = RecallLayout.Defualt;
+    
     void Start()
     {
-        
+        option = RecallLayout.Defualt;
+
     }
 
- 
-    void Update()
+ public void OnTriggerEntter2D(Collider2D collider)
     {
-        
+        if (collider.gameObject.CompareTag("Recall_Layout_Custom_Diag1"))
+        {
+            Debug.Log("Recall_Layout_Custom_Diag1 is active");
+        }
+    
+            if (collider.gameObject.CompareTag("RecallLayout_Linear_Horozontal"))
+        {
+            Debug.Log("RecallLayout_Linear_Horozontal");
+        }
     }
 }
