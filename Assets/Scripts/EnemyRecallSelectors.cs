8175e9c3d3d6842850e725bb1bd682258fc3619d
steynchris42-alt <steynchris42@gmail.com>
2026-05-12 13:05:39 +0200

    Enemy Movement Update
    completed positioning calculation for the other layouts

--- Assets/Scripts/EnemyRecallSelectors.cs
+++ Assets/Scripts/EnemyRecallSelectors.cs
@@ -15,8 +15,9 @@ public class EnemyRecallSelectors : MonoBehaviour
     public RecallLayout option = RecallLayout.Defualt;
 
     public bool Is_Defualt = false;
-    public bool Is_LinHoroz= false;
     public bool Is_LinVert = false;
+    public bool Is_LinHoroz= false;
+
     public bool Is_CusDiag1 = false;
     public bool Is_CusDiag2 = false;
 
@@ -40,7 +41,7 @@ public class EnemyRecallSelectors : MonoBehaviour
             Is_Defualt = true;
             if (Is_Defualt == true)
             {
-                Recall_Lyaout_Defualt();
+                Recall_Layout_Defualt();
             }
         }
         else
@@ -48,7 +49,15 @@ public class EnemyRecallSelectors : MonoBehaviour
             Is_Defualt = false;
         }
 
+        if (option == RecallLayout.LinearVerticle)
+            Is_LinVert = true;
+           if(Is_LinVert == true)
+        {
 
+        }
+        
+
+        
 
     }
 
@@ -92,13 +101,42 @@ public class EnemyRecallSelectors : MonoBehaviour
 
       
     }
-    public void Recall_Lyaout_Defualt()
+    public void Recall_Layout_Defualt()
     {
 
 
         EnemyMoveRev.recallStart.localPosition = new Vector3(18.74814f, 9.504487f, Z);
         EnemyMoveRev.recallP1.localPosition = new Vector3(22.49776f , -1.900897f ,Z);
         EnemyMoveRev.recallP2.localPosition = new Vector3(12.49876f, -10.45494f , Z);
+
+    }
+    
+    public void Recall_Layout_LinearVerticle()
+    {
+        EnemyMoveRev.recallStart.localPosition = new Vector3(-0.4396276f, 4.348204f, Z);
+        EnemyMoveRev.recallP1.localPosition = new Vector3(1.52f , 11.9209f , Z);
+        EnemyMoveRev.recallP2.localPosition = new Vector3(11.7895f , 1.440449f , Z);
+    }
+
+    public void Recall_Layout_LinearHorozontal()
+    {
+        EnemyMoveRev.recallStart.localPosition = new Vector3(16.68801f, -1.35449f , Z);
+        EnemyMoveRev.recallP1.localPosition = new Vector3(27.76739f , 4.317301f, Z);
+        EnemyMoveRev.recallP2.localPosition = new Vector3(8.039876f , 7.143144f, Z);
+
+    }
+    public void Recall_Layout_CustomDiag1()
+    {
+        EnemyMoveRev.recallStart.localPosition = new Vector3(-12.0592f, -4.205841f, Z);
+        EnemyMoveRev.recallP1.localPosition = new Vector3(-17.22821f, -9.939423f, Z);
+        EnemyMoveRev.recallP2.localPosition = new Vector3(-28.20658f, -10.91538f, Z);
+    }
+
+    public void Recall_Layout_CustomDiag2()
+    {
+        EnemyMoveRev.recallStart.localPosition = new Vector3(11.68844f, -3.255395f, Z);
+        EnemyMoveRev.recallP1.localPosition = new Vector3(16.51844f, -10.88988f, Z);
+        EnemyMoveRev.recallP2.localPosition = new Vector3(31.78745f, -11.86584f, Z);
 
     }
     public void Recall_Unset()
