06d4656a25836f6fcdd1f1b451ab177f47d6cfc3
steynchris42-alt <steynchris42@gmail.com>
2026-05-14 01:58:29 +0200

    enemy light progress
    Setup timer system for light change.

--- Assets/EnemyLightingControl.cs
+++ Assets/EnemyLightingControl.cs
@@ -5,22 +5,37 @@ using UnityEngine.Rendering.Universal;
 
 public class EnemyLightTypeDefault : EnemyLightingCollisionEffects
 {
-  public EnemyLightingCollisionEffects Light_Collider_Evt;
-    public void EnemyLightingDefualt()
+    public void ENemyLighting_Defualt()
     {
-        if (EnemyLighting == Enemy_Lighting_Type.Defualt)
-        {
-           
-        }
-       
+        Light_Source.intensity = 2;
     }
+ 
 }
-public class EnemyLightTypeDamaged : EnemyLightTypeDefault
+public class EnemyLightTypeDamaged : EnemyLightingCollisionEffects
 {
+    //Timer sytem for damage effect.
+    public void EnemyLighting_Damanged()
+    {
+        float Light_Damage_Timer = 1.0f;
+        Light_Damage_Timer -= Time.deltaTime;
+
+        if (Light_Damage_Timer <= 0.0f)
+        {
+            if (EnemyLighting == Enemy_Lighting_Type.Damged)
+            {
+                Light_Source.intensity = 1;
+            }
+        }
+
+        else if (Light_Damage_Timer > 0.0f)
+        {
+            return;
+        }
+    }
 
 }
 
-public class EnemyLightTypeDead : EnemyLightTypeDefault
+public class EnemyLightTypeDead : EnemyLightingCollisionEffects
 {
 
 }
@@ -28,25 +43,31 @@ public class EnemyLightTypeDead : EnemyLightTypeDefault
 public class EnemyLightingCollisionEffects : MonoBehaviour
 {
     public GameObject player_Bullets;
+
+
     public Light2D Light_Source;
+
+    
     public enum Enemy_Lighting_Type
     {
         Defualt,
         Damged,
         Dead,
     }
-   public Enemy_Lighting_Type EnemyLighting;
+    public Enemy_Lighting_Type EnemyLighting;
+
+
     public void Start()
     {
        Light_Source = GetComponent<Light2D>();
     }
-
 
     public void OnTrigggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.CompareTag("Bullets"))
         {
             EnemyLighting = Enemy_Lighting_Type.Defualt;
+
         }
 
     }
