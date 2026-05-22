933bfdcf36a27e6229cc14721363bbbfec9b3a9d
steynchris42-alt <steynchris42@gmail.com>
2026-05-22 13:59:30 +0200

    6sdrytxd

--- Assets/Scripts/EnemyNpc/Enemy1/EnemyHealth.cs
+++ Assets/Scripts/EnemyNpc/Enemy1/EnemyHealth.cs
@@ -12,6 +12,7 @@ public class EnemyHealth : MonoBehaviour
     public float DamageEffectDuration = 0.5f;
     public bool IsTakingDamage = false;
     public Light2D Enemy_Light;
+    public SpriteRenderer Enemy_SpriteRenderer;
     
      public void OnCollisionEnter2D(Collision2D collision)
     {               
@@ -32,6 +33,7 @@ public class EnemyHealth : MonoBehaviour
     {
 
        Enemy_Light.intensity = 100;
+        Enemy_SpriteRenderer.color = Color.red;
         yield return new WaitForSeconds(0.1f);
         Enemy_Player_Detection.Chasing_Light_Effect();
     }
