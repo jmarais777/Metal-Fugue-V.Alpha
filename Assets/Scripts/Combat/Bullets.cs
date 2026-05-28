77f4ae33359988da47ae8a5ce50a29a21a8dc0c1
steynchris42-alt <steynchris42@gmail.com>
2026-05-28 05:32:03 +0200

    yayyy

--- Assets/Scripts/Combat/Bullets.cs
+++ Assets/Scripts/Combat/Bullets.cs
@@ -1,4 +1,5 @@
 
+using System.Collections;
 using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -17,22 +18,29 @@ public class Bullet : MonoBehaviour
         RigBod = GetComponent<Rigidbody2D>();
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         RigBod.linearVelocity = transform.up * LinearVelocity;
+ 
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(this.gameObject);
-      
-        if (collision.gameObject.CompareTag("Scrapheap"))
+
+        StartCoroutine(Self_Destruct_Timer());
+       /* if (collision.gameObject.CompareTag("Scrapheap"))
         {
 
             Destroy(gameObject);
         }
         if (collision.gameObject.CompareTag("Enviroment"))
         {  
-            Destroy(this.gameObject); 
-        }
+            Destroy(gameObject); 
+        }*/
 
     }
-    
+    public IEnumerator Self_Destruct_Timer()
+    {
+        yield return new WaitForSeconds(0.01f);
+        Destroy(gameObject);
+        yield return null;
+    }
+
 }
